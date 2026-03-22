4a1124cddf02ad813289388bbdbf6ea9a39263b3
早川 範弥 <noriya@hayakawa.me>
2026-03-22 10:44:26 +0900

    CMake設定の更新とビルドタスクの追加、コードの整形を改善

--- include/main.hpp
+++ include/main.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "core.hpp"
 #include <iostream>
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "main.hpp"
 
-int main(int argc, char *argv[]) {
+int wmain(int argc, wchar_t *argv[]) {
+#ifdef _WIN32
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+#endif
   std::cout << core::hello();
   return 0;
 }
